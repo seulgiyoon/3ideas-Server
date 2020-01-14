b3d80b038da2ecf5fe2933fb67b1819d5e80c2cf
RamiTae <ramiTae.dev@gmail.com>
2020-01-14 22:29:48 +0900

    :seedling: adding seed file

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+const userRouter = require('./routes/user');
+const askRouter = require('./routes/ask');
+const asksRouter = require('./routes/asks');
+const answerRouter = require('./routes/answer');
+const categoryRouter = require('./routes/category');
+
+const app = express();
+const port = 5000;
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    credentials: true,
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.status(200).json('Success');
+});
+
+app.use('/user', userRouter);
+app.use('/user', askRouter);
+app.use('/user', asksRouter);
+app.use('/user', answerRouter);
+app.use('/user', categoryRouter);
+
+app.set('port', port);
+app.listen(app.get('port'), () => {
+  console.log(`app is listening in PORT ${app.get('port')}`);
+});
+
+module.exports = app;
