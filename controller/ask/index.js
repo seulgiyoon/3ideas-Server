4006685984d86abff75f28f4076f3f1a7d599da5
RamiTae <ramiTae.dev@gmail.com>
2020-01-16 21:45:47 +0900

    :construction: implement post, get to ask

--- controller/ask/index.js
+++ controller/ask/index.js
@@ -1,12 +1,64 @@
-const { questions } = require('../../models');
+const { questions, users } = require('../../models');
 
 module.exports = {
   post: (req, res) => {
-    res.status(201).json('ask post');
+    const { username, title, contents } = req.body;
+    if (!username || !title || !contents) {
+      //! username, title, contents 중 하나라도 null이면 안 됨
+      return res.status(400).json('Please send valid syntax');
+    }
+
+    users
+      .findOne({ where: { userName: username } })
+      .then(result => {
+        //? username 존재여부 확인
+        if (!result) {
+          //! username이 존재하지 않음
+          return res.status(404).json('invalid username!');
+        }
+
+        const user_id = result.id;
+        questions
+          .create({ title, contents, user_id })
+          .then(result => {
+            //* 입력에 성공하면 새로 생성된 question의 id를 response로 보내줌
+            const { id } = result;
+            res.status(201).json({ id });
+          })
+          .catch(err => res.status(400).send(err));
+      })
+      .catch(err => res.status(400).send(err));
   },
+
   get: (req, res) => {
-    res.status(200).json('ask get');
+    const { askId } = req.params;
+    if (!parseInt(askId)) {
+      //! askId가 숫자가 아님
+      return res.status(400).json('invalid API parameter');
+    }
+
+    questions
+      .findOne({
+        where: { id: askId },
+        include: [
+          {
+            model: users,
+          },
+        ],
+      })
+      .then(result => {
+        if (!result) {
+          //! 해당 id의 질문글이 없음
+          return res.status(404).json('invalid ask id');
+        }
+
+        const { id, title, contents, questionFlag, createdAt, updatedAt } = result;
+        const username = result.user.userName;
+        res.status(200).json({ id, title, contents, username, questionFlag, createdAt, updatedAt });
+      })
+      .catch(err => res.status(400).send(err));
   },
+
   patch: (req, res) => {
     res.status(200).json('ask patch');
   },
