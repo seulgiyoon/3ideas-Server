147c1289bf5261024f227d189f7b75afbc417673
TaekwanMin <alsxorhks@gmailc.om>
2020-01-18 15:07:39 +0900

    :sparkles: Implement answers.js in controllers

--- controller/answers/index.js
+++ controller/answers/index.js
@@ -1,9 +1,25 @@
 const { answers } = require('../../models');
 
 module.exports = {
+  // ? 해당 질문의 답변글 id들 보내주기 ( /answers/질문글id )
   get: (req, res) => {
     // GET /:askId
     const target = req.params.askId;
-    res.status(200).json(`asks get. params: ${target}`);
+    if (!Number(target)) {
+      return res.status(400).json('bad request!');
+    }
+
+    answers
+      .findAll({
+        where: {question_id: target},
+        attributes: ['id']
+      })
+      .then(result => {
+        if(result.length === 0) {
+          return res.status(404).json('no answers yet!')
+        }
+        res.status(200).json(result)
+      })
+      .catch(err => res.status(400).send(err))
   },
 };
