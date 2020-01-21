ebef0d7cecd89e61b7683d6bd6e5f8e55be6696b
RamiTae <ramiTae.dev@gmail.com>
2020-01-21 21:08:20 +0900

    :sparkles: Implement user data 'get' response feature

--- controller/user/index.js
+++ controller/user/index.js
@@ -1,17 +1,50 @@
-const { users } = require('../../models');
+const { users, questions, answers } = require('../../models');
 
 module.exports = {
   signup: require('./signup'),
   login: require('./login'),
   logout: require('./logout'),
+
+  //? 회원정보 보기 요청 ( /user/:userName )
   get: (req, res) => {
-    // GET: /user/userId
-    const userId = req.params.userId;
-    res.status(200).json(`user get. user id: ${userId}`);
+    const { userName } = req.params;
+
+    users
+      .findOne({
+        attributes: ['id', 'userName', 'createdAt', 'point', 'report'],
+        where: { userName },
+        include: [
+          {
+            model: questions,
+            attributes: ['id'],
+          },
+          {
+            model: answers,
+            attributes: ['id'],
+          },
+        ],
+      })
+      .then(user => {
+        if (!user) {
+          return res.status(400).json('invalid user name');
+        }
+
+        const body = user.dataValues;
+
+        body.postCount = body.questions.length;
+        delete body.questions;
+
+        body.commentsCount = body.answers.length;
+        delete body.answers;
+
+        res.status(200).json(body);
+      });
   },
+
   delete: (req, res) => {
     res.status(200).json('user delete');
   },
+
   patch: (req, res) => {
     res.status(201).json('user fetch');
   },
